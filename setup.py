bbdb223640bcd643760d84b5c363d2d3d88a8b05
brian.hoag1 <brian.hoag@paperlogic.co.jp>
2024-10-03 18:03:59 +0700

    chore: update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='PrivateSign',
-  version='0.1.0',
+  version='0.2.0',
   packages=find_packages(),
   include_package_data=True,
   description='A secure sign PDF files SDK',
