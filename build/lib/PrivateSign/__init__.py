4d624adf36da93f6454fa03b44e307464f25f82b
brian.hoag1 <brian.hoag@paperlogic.co.jp>
2024-10-04 11:01:18 +0700

    refactor

--- build/lib/PrivateSign/__init__.py
+++
@@ -1,5 +0,0 @@
-# interface.py
-from .signer import cms
-
-def sign():
-    return cms.sign()
--- setup.py
+++ setup.py
@@ -1,23 +1,39 @@
-from setuptools import setup, find_packages
+from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+import os
+
+extensions = [
+    Extension("PrivateSign.signer.cms", ["PrivateSign/signer/cms.py"]),
+    Extension("PrivateSign.signer.validate", ["PrivateSign/signer/validate.py"]),
+]
+
+# cmsModule = Extension(
+#   "PrivateSign.signer.cms",
+#   ["PrivateSign/signer/cms.py"]
+# )
+
+# validateModule = Extension("PrivateSign.signer.validate", ["PrivateSign/signer/validate.py"])
+
+# extensions = [cmsModule, validateModule]
 
 setup(
-  name='PrivateSign',
-  version='0.3.0',
-  packages=find_packages(),
-  include_package_data=True,
-  description='A secure sign PDF files SDK',
-  long_description=open('README.md').read(),
-  long_description_content_type='text/markdown',
-  author='Brian',
-  author_email='brian.hoag@paperlogic.co.jp',
-  url='https://github.com/brianhoag1/PrivateSign',
-  classifiers=[
-      'Programming Language :: Python :: 3',
-      'License :: OSI Approved :: MIT License',
-      'Operating System :: OS Independent',
-  ],
-  python_requires='>=3.6',
-  install_requires=[
-      'requests',
-  ],
+    name="PrivateSign",
+    version="0.4.0",
+    author="Brian",
+    author_email="brian.hoag@paperlogic.co.jp",
+    description="A secure sign PDF files SDK",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    ext_modules=cythonize(extensions),
+    include_package_data=True,
+    zip_safe=False,
+    package_data={
+        'PrivateSign': ['api.py'],
+        'PrivateSign.src': ['*.so', '*.pyd'],
+    },
+    exclude_package_data={
+        '': ['*.py', '*.pyc'], 
+    },
 )
