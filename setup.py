a79aeb4f1366d57337c9328fd88cc4fe00d42a2a
brian.hoag1 <brian.hoag@paperlogic.co.jp>
2024-10-04 15:09:05 +0700

    fix: add ci

--- setup.py
+++ setup.py
@@ -8,40 +8,42 @@ extensions = [
     Extension("PrivateSign.signer.validate", ["PrivateSign/signer/validate.py"]),
 ]
 
-# class build_py(_build_py):
-#   def build_package_data(self):
-#       # Exclude source files after building
-#       super().build_package_data()
-#       for package in self.packages:
-#           package_dir = self.get_package_dir(package)
-#           module_files = self.find_package_modules(package, package_dir)
-#           for (pkg, module, module_file) in module_files:
-#               src_file = os.path.join(self.build_lib, module_file)
-#               if src_file.endswith(('.py', '.pyx')) and os.path.exists(src_file):
-#                   print(f"=====> Removing {src_file}")
-#                   os.remove(src_file)
+class build_py(_build_py):
+    def build_package_data(self):
+        # Exclude source files after building
+        super().build_package_data()
+        for package in self.packages:
+            package_dir = self.get_package_dir(package)
+            module_files = self.find_package_modules(package, package_dir)
+            for (pkg, module, module_file) in module_files:
+                src_file = os.path.join(self.build_lib, module_file)
+                if src_file.endswith(('.py', '.pyx')) and not src_file.endswith(('api.py')) and os.path.exists(src_file):
+                    print(f"=====> Removing {src_file}")
+                    os.remove(src_file)
 
 setup(
     name="PrivateSign",
-    version="0.6.0",
+    version="0.7.0",
     author="Brian",
     author_email="brian.hoag@paperlogic.co.jp",
     description="A secure sign PDF files SDK",
     packages=find_packages(),
-    # cmdclass={'build_py': build_py},
+    cmdclass={'build_py': build_py},
     install_requires=[
         "requests",
     ],
     ext_modules=cythonize(extensions),
     include_package_data=True,
     zip_safe=False,
-    package_data={
-        # 'PrivateSign': ['api.py'],
-        # 'PrivateSign.src': ['*.so', '*.pyd'],
-        'PrivateSign': ['*.pyd', '*.so'],
-    },
-    exclude_package_data={
-        # '': ['*.py', '*.pyc'], 
-        'PrivateSign': ['signer/*.py'],
-    },
+    python_requires='>=3.6',
+
+    # package_data={
+    #     # 'PrivateSign': ['api.py'],
+    #     # 'PrivateSign.src': ['*.so', '*.pyd'],
+    #     'PrivateSign': ['*.pyd', '*.so'],
+    # },
+    # exclude_package_data={
+    #     # '': ['*.py', '*.pyc'], 
+    #     'PrivateSign': ['signer/*.py'],
+    # },
 )
