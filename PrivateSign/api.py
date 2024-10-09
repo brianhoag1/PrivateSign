b0077f9463d52b1bc6b06a3126852ffac4002c8f
brian.hoag1 <brian.hoag@paperlogic.co.jp>
2024-10-09 18:08:44 +0700

    fix timestamp

--- PrivateSign/api.py
+++ PrivateSign/api.py
@@ -1,4 +1,4 @@
-from .signer import sign, sign_no_timestamp
+from .signer import sign, sign_no_timestamp, timestamp
 
 def sign_document(input_file, output_file, api_token, tenant_id, cert_type = 0):
     return sign(input_file, output_file, api_token, tenant_id, cert_type = 0)
@@ -7,4 +7,4 @@ def sign_document_no_timestamp(input_file, output_file, api_token, tenant_id, ce
     return sign_no_timestamp(input_file, output_file, api_token, tenant_id, cert_type = 0)
 
 def timestamp_document(input_file, output_file, api_token, tenant_id):
-    return sign(input_file, output_file, api_token, tenant_id)
+    return timestamp(input_file, output_file, api_token, tenant_id)
